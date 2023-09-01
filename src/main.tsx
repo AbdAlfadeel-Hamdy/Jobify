78b3796f0bc57487be6c7a0c0f7db9667e36a0c1
Abd Alfadeel Hamdy <abdalfadeelh@gmail.com>
2023-09-01 15:07:05 +0300

    Add Custom Fetch

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-fetch("/api/v1/jobs")
-  .then((res) => res.json())
-  .then((data) => console.log(data));
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
