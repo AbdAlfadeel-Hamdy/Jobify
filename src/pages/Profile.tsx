09bfbb6cc4ce4660c1a57a3a7212c4a295f6b9c0
Abd Alfadeel Hamdy <abdalfadeelh@gmail.com>
2023-10-20 08:11:32 +0300

    Add Profile Page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,5 +1,63 @@
+import { ActionFunction, Form, useNavigation } from "react-router-dom";
+import { toast } from "react-toastify";
+import Wrapper from "../assets/wrappers/DashboardFormPage";
+import { FormRow } from "../components";
+import { useDashboardContext } from "../context/dashboard";
+import customFetch from "../utils/customFetch";
+import { AxiosError } from "axios";
+
+export const action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData();
+
+  const file = formData.get("avatar");
+  if (file && file instanceof File && file.size > 500000) {
+    return toast.error("Image size is too large");
+  }
+
+  try {
+    await customFetch.patch("/user/update-user", formData);
+    toast.success("Profile updated successfully");
+  } catch (error) {
+    if (error instanceof AxiosError) toast.error(error.response?.data.message);
+    else toast.error("Something went wrong");
+  }
+  return null;
+};
+
 const Profile: React.FC = () => {
-  return <div>Profile</div>;
+  const { user } = useDashboardContext();
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+  return (
+    <Wrapper>
+      <Form method="POST" className="form" encType="multipart/form-data">
+        <h4 className="form-title">Profile</h4>
+        <div className="form-center">
+          <FormRow
+            type="file"
+            name="avatar"
+            label="select an image file (Max 0.5 MB):"
+            accept="image/*"
+          />
+          <FormRow name="name" defaultValue={user.name} />
+          <FormRow
+            name="lastName"
+            label="last name"
+            defaultValue={user.lastName}
+          />
+          <FormRow type="email" name="email" defaultValue={user.email} />
+          <FormRow name="location" defaultValue={user.location} />
+          <button
+            className="btn btn-block form-btn"
+            type="submit"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? "Saving..." : "Save changes"}
+          </button>
+        </div>
+      </Form>
+    </Wrapper>
+  );
 };
 
 export default Profile;
