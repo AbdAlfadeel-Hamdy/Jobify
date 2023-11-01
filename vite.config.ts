579b94c59158f80182d3486e535132be9f711c9a
Abd Alfadeel Hamdy <abdalfadeelh@gmail.com>
2023-11-01 19:51:02 +0200

    Remove Server Proxy

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        // target: "http://localhost:5100/api",
-        target: "https://jobify-server-8qm8.onrender.com/api",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       // target: "http://localhost:5100/api",
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/api/, ""),
+  //     },
+  //   },
+  // },
 });
