e2b36262980cce5db8eebf782daf5b07f19342a3
Abd Alfadeel Hamdy <abdalfadeelh@gmail.com>
2023-11-01 19:35:45 +0200

    Add Production Server

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5100/api",
+        // target: "http://localhost:5100/api",
+        target: "https://jobify-server-8qm8.onrender.com/api",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
