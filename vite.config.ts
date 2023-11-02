7f040fb8afc83a2c08aef857df4975243d168181
Abd Alfadeel Hamdy <abdalfadeelh@gmail.com>
2023-11-02 03:50:59 +0200

    Add Environment Variables

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // ONLY For Development Environment
   server: {
     proxy: {
       "/api": {
-        target: "https://jobify-server-8qm8.onrender.com/api",
+        target: "http://localhost:5100/api",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
